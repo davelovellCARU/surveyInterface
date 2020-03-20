9060f4658faa68c112b10c75c1dbcb44a40981a1
davelovellCARU <david.lovell@churcharmy.org>
2020-03-20 10:31:59 +0000

    removing pdfVariable from pdfTable

--- app.R
+++ app.R
@@ -171,17 +171,13 @@ ui = fluidPage(splitLayout(
     pdfFiles <- dir(pdfDir, full.names =  TRUE)[dir(pdfDir, full.names = TRUE) %>% str_detect("\\.pdf$")]
     
     suppressWarnings(dir.create(paste0(pdfDir,"/output")))
-    pdfLength = pdf_length(pdfFile)
     
-
-    pdf <- tibble(pageNo = 1:pdfLength)
+    pdf <- tibble(pageNo = 1:(length(pdfFiles)))
     pdf <- pdf %>% 
       mutate(outputFile = map_chr(pageNo,
-                                  ~{paste0(pdfDir,"/pdfPages/", as.character(.), ".pdf")})) %>%
-      mutate(page = map2(pageNo, outputFile,
-                         ~ pdf_subset(pdfFile, .x, output = .y))) %>%
+                                  ~{paste0(pdfDir, as.character(.), ".pdf")})) %>%
       mutate(http = paste0("http:\\\\127.0.0.1:6056\\",
-                           str_remove(page,"^.+\\\\www\\\\")) %>% 
+                           str_remove(outputFile, "^.+\\\\www\\\\")) %>% 
                str_replace_all("\\\\","/"))
     
     ## Initialise reactiveValues :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
